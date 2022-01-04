57cfc8522fdfb381335ad3e64ac9529e1d935e41
Matt <bendermaj@gmail.com>
2022-01-04 17:16:12 -0500

    Player Movement Update
    movement/jump working correctly now
    movement was changed to axis instead of 2d vector

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,16 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D playerRB;
+
+    [Header("Movement")]
+    [SerializeField] private int speed;
+    Vector2 inputMovement = Vector2.zero;
+    private float moveHorizontal;
+    
+    [Header("Jump")]
+    [SerializeField] private int jumpForce;
+    private Vector3 jumpDirection = new Vector3(0, 2, 0);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +27,30 @@ public class PlayerMovement : MonoBehaviour
     {
         
     }
+    private void FixedUpdate()
+    {
+        //inputMovement.y = 0;
+        playerRB.velocity = new Vector2(moveHorizontal * speed, playerRB.velocity.y);
+    }
 
     public void BasicMovement(InputAction.CallbackContext context)
     {
-        Vector2 inputMovement;
+        moveHorizontal = context.ReadValue<float>();
+        Debug.Log(moveHorizontal);
+        //inputMovement = context.ReadValue<Vector2>();
+        //if (context.performed)
+        //{
+        //    Debug.Log("Move");
+        //}
+        
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
         if (context.performed)
         {
-            Debug.Log("Move");
-            inputMovement = context.ReadValue<Vector2>();
-            playerRB.velocity = inputMovement;
+            Debug.Log("Jump");
+            playerRB.AddForce(jumpForce * jumpDirection, ForceMode2D.Force);
         }
-        
     }
 }
